dc7d857dff158c6b13388a7e8027e9a95c5e1510
Mostafa Khaled <Mostafa1810751@outlook.com>
2021-05-31 21:48:36 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,23 @@
 <?php
 
-$dbHost ='localhost';
-$dbUsername ='root';
-$dbPassword ='';
-$dbDatabase ='hikelydb';
-$conn=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbDatabase);
+class DB{
+    private $dbHost;
+    private $dbUsername;
+    private $dbPassword;
+    private $dbDatabase;
+    public  $conn;
+    
+    function __construct(){
+      $this->dbHost ='localhost';
+      $this->dbUsername ='root';
+      $this->dbPassword ='';
+      $this->dbDatabase ='hikelydb';
+      $this->conn=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbDatabase);
+    }
+}
+
+
+
+
 
 ?>
