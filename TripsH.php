520ee5c6dc5697a8deae7790a5fbda07e1a81238
kareem337 <74376900+kareem337@users.noreply.github.com>
2021-06-06 05:27:41 +0200

    javascript on date format
    3shan maykhalneesh akhtar date dat

--- TripsH.php
+++ TripsH.php
@@ -57,6 +57,25 @@ $reserve = new Reserve();
 
 
 </head>
+
+<script>
+  $(function(){
+    var dtToday = new Date();
+    
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+    
+    var maxDate = year + '-' + month + '-' + day;
+    $('#currentDate').attr('min', maxDate);
+});
+  </script>
+
+  
 <body>
    <header >
    
