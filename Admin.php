2b31a95e555290df051cb203a0d1ccd9f7ae9d62
Ammar Abdelmoiz <ammar.abdelmoiz44@gmail.com>
2021-06-08 04:22:47 +0200

    AdminWork

---
+++ Admin.php
@@ -0,0 +1,18 @@
+<?php /* 
+include ("Person.php"); 
+//include ("Product.php");
+class Admin extends Person
+{
+    public function insertintodb(){
+        $x =new Product();
+    }
+
+    public function updateintodb(){
+        $x =new Product();
+    }
+
+    public function deleteintodb(){
+        $x =new Product();
+    }
+}
+*/
