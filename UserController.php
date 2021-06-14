0646c0bd6a20a7f24be192e4c316fe08b8550faa
Mostafa Khaled <Mostafa1810751@outlook.com>
2021-06-14 05:59:30 +0200

    Update UserController.php

--- UserController.php
+++ UserController.php
@@ -4,12 +4,10 @@ class UserController extends UserModel
 {
     public function insertMsg()
     {
-
         $user_id = $_SESSION["Logged_in_ID"];
         $user_name = $_SESSION["Logged_in_Name"];
         $msg = $_POST['msg'];
         $this->sendToAdmin($user_id, $user_name, $msg);
-
     }
 
     public function insertRecord()
